a164c120afb6e5b4f48cc16ff6057c0fd5556d65
Chia-Chen Wu <kevin0211722@gmail.com>
2025-11-04 22:13:28 +0000

    Code completed

--- budget_tracker.py
+++ budget_tracker.py
@@ -1,215 +1,338 @@
 
+# -----------------------------------------Task 2-----------------------------------------
 
-
-
-
-# Task 2
-
-transactions = []
-
-test1 = {
-    "type": "Expense",
-    "amount": 100.2,
-    "description": "gas",
-    "category": "daily"
-}
-
-test2 = {
-    "type": "Expense",
-    "amount": 200.35,
-    "description": "tution",
-    "category": "education"
-}
-
-test3 = {
-    "type": "Expense",
-    "amount": 10,
-    "description": "drinks",
-    "category": "food"
-}
-
-test4 = {
-    "type": "Expense",
-    "amount": 110,
-    "description": "lunch",
-    "category": "food"
-}
-
-test5 = {
-    "type": "Expense",
-    "amount": 5,
-    "description": "shampo",
-    "category": "daily"
-}
-
-transactions.append(test1)
-transactions.append(test2)
-transactions.append(test3)
-transactions.append(test4)
-transactions.append(test5)
+# Create a list to store all transactions
+transactions = [] 
 
 def display_menu():
-    option_1 = "1. Add Income"
-    option_2 = "2. Add Expense"
-    option_3 = "3. List Transactions"
-    option_4 = "4. Delete Transaction"
-    option_5 = "5. View Overall Summary"
-    option_6 = "6. View Summary by Category"
-    option_7 = "7. Exit"
+    # Display the main menu and let the user select a function.
+    # The user can add income/expenses and print transaction details.
+    # Select 7 (Exit) to terminate the program.
+
+    # Args:
+    #     None
+    # Returns:
+    #     None
 
     exit = False
-    while exit == False:
+    
+    # Keep looping until the user exits the program.
+    while exit == False:    
         print("\n--- Personal Budget Tracker ---")
-        print(option_1)
-        print(option_2)
-        print(option_3)
-        print(option_4)
-        print(option_5)
-        print(option_6)
-        print(option_7)
+        print("1. Add Income")
+        print("2. Add Expense")
+        print("3. List Transactions")
+        print("4. Delete Transaction")
+        print("5. View Overall Summary")
+        print("6. View Summary by Category")
+        print("7. Exit")
 
-        while True:
+        # This loop ensures that only valid selections are accepted.
+        while True:         
             try:
-                inp = int(input("Please enter your choice:"))
+                # Prompt the user to select an option.
+                inp = int(input("\nPlease enter your choice:"))     
                 if inp > 7 or inp < 1:
                     raise ValueError()
                 break
             except:
-                print("Take 1~ 7 only")
-                
-        
-        if inp == 2:
+                # Show an error message.
+                print("Accept only options 1~7.")                   
+  
+        if inp == 1:        # Add Income
+            pass
+        elif inp == 2:      # Add Expenses
             add_transaction(transactions,"Expense")
-        elif inp == 3:
+        elif inp == 3:      # List Transactions
             list_transactions(transactions)
-        elif inp == 4:
+        elif inp == 4:      # Delete Transaction
             delete_transaction(transactions)
-        elif inp == 5:
+        elif inp == 5:      # View Overall Summary
             print_overall_summary(calculate_summary(transactions))
-        elif inp == 6:
+        elif inp == 6:      # View Summary by Category
             print_summary_by_category(transactions)
-        elif inp == 7:
+        elif inp == 7:      # Exit
             exit = True
 
+    # Termination message
+    print("\nThanks for using :D")  
     return
 
 
-# Task 3
+#-----------------------------------------Task 3-----------------------------------------
 
 def add_transaction(current_transactions, transaction_type):
-    if transaction_type == "Income":
-        print("1515151")
-    elif transaction_type == "Expense":
-        while True:
-            try:
-                amount = float(input("Enter amount:"))
-                if amount < 0:
-                    raise ValueError()
-                break
-            except:
-                print("Invalid amount. Please enter a positive number.")
-                return
+    # Add a transaction to the transactions list.
 
+    # Args:
+    #     current_transactions (transactions)
+    #     transaction_type ("Income" or "Expense")
+    # Returns:
+    #     None
+
+    # Handle income transactions.
+    if transaction_type == "Income":
+        pass
+        
+    # Handle expense transactions.
+    elif transaction_type == "Expense":
+        # Ensure only positive numbers are accepted.
+        # Return early if an invalid value is entered.
+        try:
+            # Prompt the user to enter an amount.
+            amount = float(input("Enter amount:"))
+            if amount < 0:
+                raise ValueError()
+        except:
+            # Print an error message and return.
+            print("Invalid amount. Please enter a positive number.")
+            return
+
+        # Prompt the user to enter a description and category
         description = input("Enter description:")
         category = input("Enter category:")
 
+        # Create a temporary transaction record.
         transac = {
             "type": transaction_type,
             "amount": amount,
             "description": description,
             "category": category
         }
-        transactions.append(transac)
+        current_transactions.append(transac)
 
     return
 
-# Task 4
+#-----------------------------------------Task 4-----------------------------------------
 
 def list_transactions(transactions):
-    if len(transactions) == 0:
+    # List all transaction records in transactions list.
+
+    # Args:
+    #     transactions(transactions)
+    # Returns:
+    #     None
+
+    # If there are no transactions, print a message instead.
+    if len(transactions) == 0:                 
         print("No transactions found.")
     else:
         print("\n--- All Transactions ---")
-        for i in range(len(transactions)):
+        # Print all transactions.
+        for i in range(len(transactions)):      
             print(f"{i+1}. [{transactions[i]["type"]}] -  £{transactions[i]["amount"]} - {transactions[i]["description"]} (Category: {transactions[i]["category"]})")
     
     return
 
-# Task 5
+#-----------------------------------------Task 5-----------------------------------------
 
 def delete_transaction(transactions):
-    if len(transactions) == 0:
+    # Prompt the user to delete a transaction.
+
+    # Args:
+    #     transactions(transactions)
+    # Returns:
+    #     None
+
+    # Return if there are no transactions.
+    if len(transactions) == 0:          
         return
     
-    list_transactions(transactions)
+    # List all transaction records.
+    list_transactions(transactions)     
     
+    # Ask the user which transaction to delete.
+    # Create an index for the record to be deleted.
     dele = 0
-    while True:
-        try:
-            dele = int(input("Which of the following transactions would you like to delete?"))
-            if dele < 1 or dele> len(transactions):
-                raise ValueError()
-            break
-        except:
-            print("Invalid index.")
-            return
-    
-    deleteIndex = dele-1
-    transactions.remove(transactions[deleteIndex])
 
+    # Ensure that only a valid index is accepted.
+    try:    
+        dele = int(input("Which of the following transactions would you like to delete?"))
+        if dele < 1 or dele> len(transactions):
+            raise ValueError()
+    except:
+        print("Invalid index.")
+        return                        
+        
+    # Decreases index by one so the correct record can be deleted
+    deleteIndex = dele-1                
+    
+    transactions.remove(transactions[deleteIndex])
+    
+    # Deletion successful message
     print(f"No.{dele} transaction has been deleted")
 
     return 
 
 
-# Task 6
+#-----------------------------------------Task 6-----------------------------------------
 
 def calculate_summary(transactions):
+    # Return a dictionary containing the total income, total expense, and net balance.
+    
+    # Args:
+    #     transactions(transactions)
+    # Returns:
+    #     summary_data
+    
+
+    # Initialises the dictionary to be returned
     summary_data = {
         "Income": 0,
         "Expense": 0,
         "Net Balance": 0
     }
 
+    # Calculates the total expense and income and store them in summary_data
     for i in transactions:
         if i["type"] == "Expense":
             summary_data["Expense"] = summary_data["Expense"] + i["amount"]
         elif i["type"] == "Income":
             summary_data["Income"] = summary_data["Income"] + i["amount"]
-    summary_data["Net Balance"] = summary_data["Income"] - summary_data["Expense"]
+    
+    # Net Balance = Total Income - Total Expense
+    summary_data["Net Balance"] = round(summary_data["Income"] - summary_data["Expense"],2)
 
     return summary_data
 
 
 def print_overall_summary(summary_data):
+    # Print the data in summary_data.
+    
+    # Args:
+    #     summary_data, possibly from calculate_summary(transactions)
+    # Returns:
+    #     None
+
+    # Prints overall summary
     print("\n--- Overall Summary ---")
     for key, values in summary_data.items():
         print(f"{key}: £{values}")
     
     print("-----------------------")
+
     return
 
+
 def print_summary_by_category(transactions):
-    
-    print("\n--- Expenses by Category ---")
+    # Calculate the total income and expense for each category.
+    # Print the results.
 
-    dic = {}
+    # Args:
+    #     transactions(transactions)
+    # Returns:
+    #     None
+
+    # Creates a temporary dictionary for results
+    expense = {}    
+    income = {}
     for i in transactions:
-       if dic.get(i.get("category")) == None:
-           dic[i.get("category")] = 0
+       # Store the amount in the expense dictionary.
+       if i.get("type") == "Expense":
+           # If the category does not exist, create it.
+           if expense.get(i["category"]) == None:
+               expense[i["category"]] = 0
+           # Accumulate the total for this category.
+           expense[i["category"]] = expense[i["category"]] + i.get("amount")
 
-       dic[i.get("category")] = dic[i.get("category")] + i.get("amount")
-       
-    for key, value in dic.items():
+       # Store the amount in the income dictionary.
+       elif i.get("type") == "Income":
+           # If the category does not exist, create it.
+           if income.get(i["category"]) == None:
+               income[i["category"]] = 0
+           # Accumulate the total for this category.
+           income[i["category"]] = income[i["category"]] + i.get("amount")
+
+    # Print the expense summary.
+    print("\n--- Expenses by Category ---")
+    for key, value in expense.items():
+        print(f"{key}: £{value}")
+
+    # Print the income summary.
+    print("\n--- Income by Category ---")
+    for key, value in income.items():
         print(f"{key}: £{value}")
 
     print("----------------------------")
 
     return
 
-    
-    
-    
+# -----------------------------------------Testing Data-----------------------------------
 
+Ex1 = {
+    "type": "Expense",
+    "amount": 12.61,
+    "description": "Amazon books: C++ Primer",
+    "category": "Education"
+}
+
+Ex2 = {
+    "type": "Expense",
+    "amount": 2500.99,
+    "description": "tution",
+    "category": "Education"
+}
+
+Ex3 = {
+    "type": "Expense",
+    "amount": 1.69,
+    "description": "Drinks",
+    "category": "Food"
+}
+
+Ex4 = {
+    "type": "Expense",
+    "amount": 10.16,
+    "description": "lunch",
+    "category": "Food"
+}
+
+Ex5 = {
+    "type": "Expense",
+    "amount": 2,
+    "description": "Shampo",
+    "category": "Groceries"
+}
+
+Ex6 = {
+    "type": "Expense",
+    "amount": 8,
+    "description": "O2 mobile",
+    "category": "Network"
+}
+
+Ic1 = {
+    "type": "Income",
+    "amount": 1500,
+    "description": "Oct salary",
+    "category": "Salary"
+}
+
+Ic2 = {
+    "type": "Income",
+    "amount": 35.4,
+    "description": "Amazon",
+    "category": "Refund"
+}
+
+Ic3 = {
+    "type": "Income",
+    "amount": 500,
+    "description": "Rent",
+    "category": "Other"
+}
+
+
+transactions.append(Ex1)
+transactions.append(Ex2)
+transactions.append(Ex3)
+transactions.append(Ex4)
+transactions.append(Ex5)
+transactions.append(Ex6)
+
+transactions.append(Ic1)
+transactions.append(Ic2)
+transactions.append(Ic3)
+#----------------------------------------------------------------------------------------
 
 display_menu()
